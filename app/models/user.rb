78d74730866950f888b806b9b0a5b0e35a880a3e
davidricardo <david.ricardo.1991@gmail.com>
2023-08-30 12:07:31 -0400

    seeds completed

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :user_decks
   has_many :decks, through: :user_decks
+  validates :first_name, presence: true
+  validates :last_name, presence: true
 end
