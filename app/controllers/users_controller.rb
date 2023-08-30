fa46bef8a07c3a7e23b38e22127ec7db5795c4fd
davidricardo <david.ricardo.1991@gmail.com>
2023-08-30 15:45:07 -0400

    Add dotenv-rails

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
   def avatar
-    params.require(:article).permit(:title, :body, :photo)
+    params.require(:users).permit(:email, :password, :avatar, :first_name, last_name:)
   end
 end
