bb94502700754f0dac61827b5b2b553c9eaa1aa7
davidricardo <david.ricardo.1991@gmail.com>
2023-09-07 13:10:47 -0400

    on going

--- app/javascript/controllers/game_controller.js
+++ app/javascript/controllers/game_controller.js
@@ -138,7 +138,7 @@ export default class extends Controller {
           headers: headers,
           method: "POST"
         })
-        this.#endGame(this.congratulationsTemplateTarget.innerHTML)
+        this.#endGame(this.loserTemplateTarget.innerHTML)
         return
 
       }
