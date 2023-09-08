ec74e85d247ae5b1e489103513ece84c2e978ad5
frederic charest <fredcharest2@gmail.com>
2023-09-08 14:32:20 -0400

    Cannot start game until both players are in logic implemented

--- app/channels/racetrack_channel.rb
+++ app/channels/racetrack_channel.rb
@@ -1,6 +1,7 @@
 class RacetrackChannel < ApplicationCable::Channel
   def subscribed
     stream_for params[:id]
+    broadcast_to(params[:id], { playerId: params[:playerId], action: "appear" })
   end
 
   def unsubscribed
@@ -8,6 +9,14 @@ class RacetrackChannel < ApplicationCable::Channel
   end
 
   def receive(data)
+    broadcast_to(params[:id], data)
+  end
+
+  def progress(data)
+    broadcast_to(params[:id], data)
+  end
+
+  def start_game(data)
     broadcast_to(params[:id], data)
   end
 end
